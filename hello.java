d456e342bf320313cd6756ca2cea30ce8df18bf0
SakthyBalan <sakthybalan.mc21@bitsathy.ac.in>
2024-09-06 16:01:49 +0530

    First Commit

---
+++ hello.java
@@ -0,0 +1,19 @@
+import java.lang.System;
+import java.util.Scanner;
+class hello{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size=scan.nextInt();
+        int find=(size/2)-1;
+        int []num=new int[size];
+        for(int i=0;i<size;i=i+1){
+            num[i]=scan.nextInt();
+        }
+        for(int i=0;i<size;i=i+1){
+            if(find==i){
+                System.out.println(num[i]);
+            }
+        }
+       }
+       
+}
